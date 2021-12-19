e800ffd0277b6ed24c0bc44fe37f995ca12a7a07
Yash P <yashpatekar18@gmail.com>
2021-12-19 13:35:12 +0530

    Controller changes.

--- TaskApp/src/main/java/com/example/demo/controller/TaskController.java
+++ TaskApp/src/main/java/com/example/demo/controller/TaskController.java
@@ -3,6 +3,8 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,27 +27,27 @@ public class TaskController {
 	private TaskService taskService;
 
 	@PostMapping("task")
-	public Task addTask(@RequestBody Task task) {
-		return this.taskService.createTask(task);
+	public ResponseEntity<Task> addTask(@RequestBody Task task) {
+		return new ResponseEntity<Task>(this.taskService.createTask(task), HttpStatus.OK);
 	}
 
 	@GetMapping("tasks")
-	public List<Task> getAllTasks() {
-		return this.taskService.readAllTasks();
+	public ResponseEntity<List<Task>> getAllTasks() {
+		return new ResponseEntity<List<Task>>(this.taskService.readAllTasks(), HttpStatus.OK);
 	}
 
 	@GetMapping("task/{taskId}")
-	public Task getTaskById(@PathVariable long taskId) {
-		return this.taskService.readTaskById(taskId);
+	public ResponseEntity<Task> getTaskById(@PathVariable long taskId) {
+		return new ResponseEntity<Task>(this.taskService.readTaskById(taskId), HttpStatus.OK);
 	}
 
 	@PutMapping("task/{taskId}")
-	public Task updateTask(@PathVariable long taskId, @RequestBody Task task) {
-		return this.taskService.updateTask(taskId, task);
+	public ResponseEntity<Task> updateTask(@PathVariable long taskId, @RequestBody Task task) {
+		return new ResponseEntity<Task>(this.taskService.updateTask(taskId, task), HttpStatus.OK);
 	}
 
 	@DeleteMapping("task/{taskId}")
-	public String deleteTask(@PathVariable long taskId) {
-		return this.taskService.deleteTask(taskId);
+	public ResponseEntity<String> deleteTask(@PathVariable long taskId) {
+		return new ResponseEntity<String>(this.taskService.deleteTask(taskId), HttpStatus.OK);
 	}
 }
